5074e5d7809a7d5eeaa3c460080a0889170884d6
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-10-13 21:37:20 -0400

    Complete BootcampX SQL Queries 3 - Group By and fix improper capitalization of aggregate functions in "SQL Queries 2"'

---
+++ 3_queries_group-by/15_students_with_lower_avg_assignment_completion_time.sql
@@ -0,0 +1,10 @@
+SELECT students.name AS student, 
+       avg(assignment_submissions.duration) AS average_assignment_duration,
+       avg(assignments.duration) AS average_estimated__duration
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+JOIN assignments ON assignments.id = assignment_submissions.assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration ASC;
